19c0ac32f25394391ede7b189e41437a127a2abe
Sam Hall <mann.samantha11@gmail.com>
2021-02-22 22:27:32 -0500

    commit tree a488a343d8a3137a8e55674ac31a8fe6abc725c1

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
+import {name, image, city} from "../data/data.js"
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name ={name} city ={city}/>
+      <About image ={image}/>
     </div>
   );
 }
